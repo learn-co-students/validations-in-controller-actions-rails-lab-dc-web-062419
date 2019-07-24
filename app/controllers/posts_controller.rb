a7837c5d5e0f20312632568865d366b719338fe7
github username <github email address>
2019-07-24 18:16:42 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,22 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+
+  end
+
+  def new
+    @post = Post.new
   end
 
   def edit
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get "/authors/:id", to: "authors#show", as: "author"
   post "/authors", to: "authors#create"
 
+  get "/posts/new", to: "posts#new"
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  post "/posts", to: "posts#create"
   patch "/posts/:id", to: "posts#update"
 end
